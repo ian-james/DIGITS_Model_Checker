9aae445c22fe73c0f7b9a8d1676b67cd88a8f23f
ian-james <jamey.fraser@gmail.com>
2024-09-24 20:14:16 -0300

    Adding a streamlit application that will allow filtering of the final datatable to view specific finger, joint, view, pose and and different columns. It is a general application and works on other tables as well.

---
+++ src/streamlit_final_table_review.py
@@ -0,0 +1,190 @@
+# This program demonstrates a streamlit UI to display a table of data that can be filtered and downloaded.
+
+import os
+import streamlit as st
+import pandas as pd
+import time
+import logging
+from log import set_log_level
+import argparse
+
+import numpy as np
+import plotly.express as px
+
+from fps_timer import FPS
+import matplotlib.pyplot as plt
+
+from streamlit_utils import save_uploadedfile, download_dataframe, display_download_buttons
+import project_settings as ps
+
+import convert_mediapipe_index as convert_mp
+from file_utils import setupAnyCSV
+
+from stats_functions import compute_statistics
+
+def page_setup():
+    st.set_page_config(page_title="Mediapipe Analysis", page_icon=":bar_chart:", layout="wide")
+    st.title("Mediapipe Analysis")
+    st.write("### This tools is designed to analyze data from a CSV file.")
+
+def setup_sidebar():
+
+    st.sidebar.title('Select File')
+    uploaded_file = st.sidebar.file_uploader("Upload a dataframe file (csv)", type=["csv"])
+
+    mediapipe_expander = st.sidebar.expander("## User Options", expanded=True)
+    mediapipe_expander.markdown("## Program Options")
+    mode_src = mediapipe_expander.selectbox("Select the mode", ["None","Graph Data"])
+
+
+    ################################################################################
+    # Debug Options
+    debug_expander = st.sidebar.expander("## Developer Options", expanded=False)
+    debug_expander.markdown("## Debugging Controls")
+
+    debug_levels = ['debug', 'info', 'warning', 'error', 'critical']
+    debug = debug_expander.selectbox("Set the logging level", debug_levels)
+
+    return uploaded_file, mode_src, debug
+
+@st.cache_data
+def load_data(filename):
+    df = setupAnyCSV(filename)
+    # cols = convert_mp.convert_all_columns_to_friendly_name(df,[])
+    # df.columns = cols
+    return df
+
+def setup_file_upload(uploaded_file):
+    # Streamlit application starts her
+    if(uploaded_file):
+        st.write("### Data Preview")
+        df = load_data(uploaded_file)
+
+        if(df is None):
+            st.write("### Error: Unable to read the file.")
+            return
+
+        st.expander("Show Data", expanded=False).write(df)
+        cols = df.columns.tolist()
+
+        # Streamlit UI
+        st.write("### Select Columns to Graph")
+
+        # Multiselect for x-axis columns
+        options_x_axis = st.multiselect('Select columns to plot on the x-axis', cols, key="x_axis")
+
+        # Multiselect for y-axis columns (limit to 4 selections)
+        options = st.multiselect('Select columns to plot', cols, max_selections=9, key="label_axis")
+
+        # Limit selection to 4 columns for both x and y axes
+        if len(options_x_axis) > 4:
+            st.warning('Please select no more than 4 x-axis columns.')
+            options_x_axis = options_x_axis[:4]
+
+        if len(options) > 4:
+            st.warning('Please select no more than 4 y-axis columns.')
+            options = options[:4]
+
+        # Initialize df_filtered as df
+        df_filtered = df.copy()
+
+        # Range slider or multiselect for x-axis filtering
+        if options_x_axis:
+            for opt in options_x_axis:
+                is_numeric = pd.api.types.is_numeric_dtype(df[opt])
+                if is_numeric:
+                    min_range, max_range = st.slider(f'Select range for {opt} values',
+                                                    float(df[opt].min()),
+                                                    float(df[opt].max()),
+                                                    (df[opt].min(), df[opt].max()))
+
+                    # Filter the data based on the selected range
+                    df_filtered = df_filtered[(df_filtered[opt] >= min_range) & (df_filtered[opt] <= max_range)]
+                else:
+                    # Create a multi-select box for non-numeric values
+                    unique_values = df[opt].unique()
+                    selected_values = st.multiselect(f"Select {opt} values", unique_values)
+
+                    # Filter the data based on the selected values
+                    if selected_values:
+                        df_filtered = df_filtered[df_filtered[opt].isin(selected_values)]
+
+        # Filter Y-axis columns (range or multiselect based on type)
+        for opt in options:
+            is_numeric = pd.api.types.is_numeric_dtype(df[opt])
+            if is_numeric:
+                min_range, max_range = st.slider(f'Select range for {opt} values',
+                                                float(df[opt].min()),
+                                                float(df[opt].max()),
+                                                (df[opt].min(), df[opt].max()))
+
+                # Filter the data based on the selected range
+                df_filtered = df_filtered[(df_filtered[opt] >= min_range) & (df_filtered[opt] <= max_range)]
+                
+               
+            else:
+                # Create a multi-select box for non-numeric values
+                unique_values = df[opt].unique()
+                selected_values = st.multiselect(f"Select {opt} values", unique_values)
+
+                # Filter the data based on the selected values
+                if selected_values:
+                    df_filtered = df_filtered[df_filtered[opt].isin(selected_values)]
+            
+            # Select only columns that are in options
+            df_filtered = df_filtered[options_x_axis + options]
+
+        st.expander("Show Filtered Data", expanded=False).write(df_filtered)
+
+        # Checkbox to indicate if y-axis range should be changed
+        yaxis_change = st.checkbox("Change y-axis range", value=False, key="yaxis_change")
+
+        # Plotting the selected columns
+        if options_x_axis and options:
+            # Create a new column that concatenates the selected x-axis column values
+            df_filtered['Group'] = df_filtered[options_x_axis].astype(str).agg(', '.join, axis=1)
+
+            # Group the data by the concatenated group values and calculate the mean
+            df_grouped = df_filtered.groupby('Group')[options].mean().reset_index()
+
+            # Create a bar chart for the grouped data
+            fig = px.bar(df_grouped, 
+                        x='Group',           # Use the concatenated group as x-axis
+                        y=options,           # Y-axis columns
+                        barmode='group',     # Group bars for different y-columns
+                        labels={'Group': ', '.join(options_x_axis)})  # Dynamic label for x-axis
+            
+            # Update the layout for better visualization of grouped categories
+            fig.update_layout(
+                xaxis_title=", ".join(options_x_axis),  # Show all group columns on x-axis
+                yaxis_title="Y Values",
+                xaxis={'categoryorder':'total descending'},  # Order the categories if needed
+                barmode='group'  # Group bars together for different Y columns
+            )
+
+            # Display the plot
+            st.plotly_chart(fig, use_container_width=True)
+
+def setup_statistics(df):
+    stats_df = compute_statistics(df, exclude_columns=['handedness','timestamp','filename'])
+    if(stats_df is not None):
+        st.write("### Statistics")
+        st.write(stats_df)
+
+def main():
+
+    project_directory = "/home/jame/Projects/Western/Western_Postdoc/Datasets/Processed_Videos/analysis/nh_1_md_0.5_mt_0.5_mp_0.5/test/"
+
+    # Setup Logging
+    set_log_level("info")
+    logging.info("Starting Program")
+
+    page_setup()
+
+    uploaded_file, mode_src, debug  = setup_sidebar()
+
+    if(uploaded_file):
+        setup_file_upload(os.path.join(project_directory,uploaded_file.name))
+
+if __name__ == '__main__':
+    main()
